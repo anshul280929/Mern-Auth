7ae77b411522bb2f3c8c97474bc00ebfa7b3f34f
Anshul <anshulbhaskar50@gmail.com>
2025-07-25 22:17:01 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
+import connectDB from './config/monodb.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+app.get('/', (req, res) => res.send('Server is running fine!'));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
