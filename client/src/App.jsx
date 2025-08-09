531b70dcc4fb108b282cf5bf7564bcdce54aa7a5
Anshul <anshulbhaskar50@gmail.com>
2025-08-09 16:18:43 +0530

    React Toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,14 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   return (
     <div>
+      <ToastContainer/>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/email-verify' element={<EmailVerify/>}/>
         <Route path='/reset-password' element={<ResetPassword/>}/>
